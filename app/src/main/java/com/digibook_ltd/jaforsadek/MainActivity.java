f26de35fbb48996e7a1f9573c09885be94fde01b
naim-ahammed <125636794+naim-ahammed@users.noreply.github.com>
2023-04-10 10:51:24 +0600

    Web App

---
+++ app/src/main/java/com/digibook_ltd/jaforsadek/MainActivity.java
@@ -0,0 +1,221 @@
+package com.digibook_ltd.jaforsadek;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.ConnectivityManager;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    /*
+    >>Source Code provided by
+    >>Jubayer Hossain [https://www.facebook.com/JubayerHossainbd]
+    >>Please don't forget to put a review on my site [www.bongoacademy.com]
+    >>Share my course with your friends on Facebook
+    >>Your positive words help me doing even better
+     */
+
+
+    // init variable here
+    WebView webView;
+    LinearLayout layNonet;
+    ProgressBar progressBar;
+    LinearLayout layRoot;
+    String USER_AGENT_ = "Mozilla/5.0 (Linux; Android 4.1.1; Galaxy Nexus Build/JRO03C) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Mobile Safari/535.19";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        progressBar = findViewById(R.id.progressBar);
+        layRoot = findViewById(R.id.layRoot);
+        layNonet = findViewById(R.id.layNonet);
+
+
+        //Creating webView programitcally which supports media player and js automatically like a browser
+        webView = new WebView(MainActivity.this);
+        webView.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT));
+        webView.setFitsSystemWindows(false); // your preferences
+        webView.setVerticalScrollBarEnabled(false); // your preferences
+        webView.setPadding(15,15,15,15); // your preferences
+        layRoot.addView(webView);
+
+        // Enabling some setting so that browser can work properly
+        webView.getSettings().setUserAgentString(USER_AGENT_);
+        webView.getSettings().setLoadsImagesAutomatically(true);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setAppCacheEnabled(true);
+        webView.getSettings().setAllowFileAccess(true);
+        webView.getSettings().setLoadWithOverviewMode(true);
+        webView.getSettings().setUseWideViewPort(true);
+        webView.getSettings().setPluginState(WebSettings.PluginState.ON);
+        webView.setWebViewClient(new HelloWebViewClient());
+        webView.setWebChromeClient(new ChromeClient());
+        webView.setWebViewClient(new WebViewClient());
+
+
+        webView.loadUrl(getString(R.string.website_link));
+
+
+        if(!isNetworkAvailable(MainActivity.this)){
+            webView.setVisibility(View.GONE);
+            layNonet.setVisibility(View.VISIBLE);
+        }
+        else{
+            webView.setVisibility(View.VISIBLE);
+            layNonet.setVisibility(View.GONE);
+        }
+
+
+
+
+    } // onCreate Ends here  >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+    public static boolean isNetworkAvailable(Context context) {
+        return ((ConnectivityManager) context
+                .getSystemService(Context.CONNECTIVITY_SERVICE))
+                .getActiveNetworkInfo() != null;
+    }
+
+    // =================================================
+
+
+
+    //================================================= defining custom class (HelloWebViewClient)
+    private class HelloWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+
+    }
+    //=================================================
+
+
+
+    //================================================= defining custom class (ChromeClient)
+    private class ChromeClient extends WebChromeClient {
+        // Defining some variables
+
+
+        private View mCustomView;
+        private CustomViewCallback mCustomViewCallback;
+        protected FrameLayout mFullscreenContainer;
+        private int mOriginalOrientation;
+        private int mOriginalSystemUiVisibility;
+        ChromeClient() {}
+
+        @Override
+        public void onProgressChanged(WebView view, int newProgress) {
+            super.onProgressChanged(view, newProgress);
+
+            if(newProgress >= 100){
+                // Page loading finish
+                progressBar.setVisibility(View.GONE);
+
+            }else{
+                progressBar.setVisibility(View.VISIBLE);
+            }
+        }
+
+
+        public Bitmap getDefaultVideoPoster()
+        {
+            if (mCustomView == null) {
+                return null;
+            }
+            return BitmapFactory.decodeResource(getApplicationContext().getResources(), 2130837573);
+        }
+
+        public void onHideCustomView()
+        {
+            ((FrameLayout)getWindow().getDecorView()).removeView(this.mCustomView);
+            this.mCustomView = null;
+            getWindow().getDecorView().setSystemUiVisibility(this.mOriginalSystemUiVisibility);
+            setRequestedOrientation(this.mOriginalOrientation);
+            this.mCustomViewCallback.onCustomViewHidden();
+            this.mCustomViewCallback = null;
+        }
+
+        public void onShowCustomView(View paramView, CustomViewCallback paramCustomViewCallback)
+        {
+            if (this.mCustomView != null)
+            {
+                onHideCustomView();
+                return;
+            }
+            this.mCustomView = paramView;
+            this.mOriginalSystemUiVisibility = getWindow().getDecorView().getSystemUiVisibility();
+            this.mOriginalOrientation = getRequestedOrientation();
+            this.mCustomViewCallback = paramCustomViewCallback;
+            ((FrameLayout)getWindow().getDecorView()).addView(this.mCustomView, new FrameLayout.LayoutParams(-1, -1));
+            getWindow().getDecorView().setSystemUiVisibility(3846 | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+        }
+    }
+    // Custom class ends here #############################
+
+
+
+
+
+
+
+
+    private static final int TIME_INTERVAL = 2000; // # milliseconds, desired
+    private long mBackPressed;
+
+    // When user click bakpress button this method is called
+    @Override
+    public void onBackPressed() {
+
+        // When landing in home screen
+        if (!webView.canGoBack()) {
+
+            if (mBackPressed + TIME_INTERVAL > System.currentTimeMillis()) {
+
+                Intent intent = new Intent(Intent.ACTION_MAIN);
+                intent.addCategory(Intent.CATEGORY_HOME);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+
+            } else {
+
+                Toast.makeText(getBaseContext(), "Press again to exit",
+                        Toast.LENGTH_SHORT).show();
+            }
+
+            mBackPressed = System.currentTimeMillis();
+
+        } else{
+            webView.goBack();
+        }
+
+
+    } // end of onBackpressed method
+
+    //#############################################################################################
+
+
+
+
+
+
+// MainActivity ends here -------------------------------------#
+}
